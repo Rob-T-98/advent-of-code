edc46b6b066e469da751ace928bbd1be353a38a0
Robert Thompson <robertpt@hotmail.co.uk>
2022-12-06 11:48:35 +0000

    computation time

--- 2022/Day 5/index.js
+++ 2022/Day 5/index.js
@@ -1,47 +1,59 @@
-const fs = require('fs');
+var times = [];
+var j = 0;
+while(j<=100){
+    var hrstart = process.hrtime()
+    const fs = require('fs');
 
-var stack = [
-    ["W","D","G","B","H","R","V"],
-    ["J","N","G","C","R","F"],
-    ["L","S","F","H","D","N","J"],
-    ["J","D","S","V"],
-    ["S","H","D","R","Q","W","N","V"],
-    ["P","G","H","C","M"],
-    ["F","J","B","G","L","Z","H","C"],
-    ["S","J","R"],
-    ["L","G","S","R","B","N","V","M"]
-]
+    var stack = [
+        ["W","D","G","B","H","R","V"],
+        ["J","N","G","C","R","F"],
+        ["L","S","F","H","D","N","J"],
+        ["J","D","S","V"],
+        ["S","H","D","R","Q","W","N","V"],
+        ["P","G","H","C","M"],
+        ["F","J","B","G","L","Z","H","C"],
+        ["S","J","R"],
+        ["L","G","S","R","B","N","V","M"]
+    ]
 
-const allFileContents = fs.readFileSync('commands.txt','utf-8');
+    const allFileContents = fs.readFileSync('commands.txt','utf-8');
 
-allFileContents.split(/\r?\n/).forEach(line =>  {
-    const regex = /\d/g;
-    var matches = line.match(regex);
-    if(matches.length==4)
+    allFileContents.split(/\r?\n/).forEach(line =>  {
+        const regex = /\d/g;
+        var matches = line.match(regex);
+        if(matches.length==4)
+        {
+            var num = matches[0]+""+matches[1];
+            var numToMove = Number(num);
+            var colFrom = matches[2];
+            var colTo = matches[3];
+        }else{
+            var numToMove = matches[0];
+            var colFrom = matches[1];
+            var colTo = matches[2];
+        }
+        
+        
+        for(let i = 0; i< numToMove; i++)
+        {
+            var column = stack[colFrom-1];
+            var lastItem = column.at(-1);
+            stack[colTo-1].push(lastItem);
+            stack[colFrom-1].splice(column.length -1,1)
+        }
+    });
+
+    var answer = ""
+    for(const col of stack)
     {
-        var num = matches[0]+""+matches[1];
-        var numToMove = Number(num);
-        var colFrom = matches[2];
-        var colTo = matches[3];
-    }else{
-        var numToMove = matches[0];
-        var colFrom = matches[1];
-        var colTo = matches[2];
+        answer = answer+ col.at(-1)
     }
-    
-    
-    for(let i = 0; i< numToMove; i++)
-    {
-        var column = stack[colFrom-1];
-        var lastItem = column.at(-1);
-        stack[colTo-1].push(lastItem);
-        stack[colFrom-1].splice(column.length -1,1)
-    }
-});
-
-var answer = ""
-for(const col of stack)
-{
-    answer = answer+ col.at(-1)
+    hrend = process.hrtime(hrstart)
+    times.push(hrend[1] / 1000000);
+    j++;
 }
-console.log(answer);
+
+const sum = times.reduce((a, b) => a + b, 0);
+const avg = (sum / times.length) || 0;
+
+console.log(avg+"ms");
